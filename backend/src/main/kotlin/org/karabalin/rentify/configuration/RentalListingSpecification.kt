c441bf9fe0b912a57fc2d25901928b4474832c71
Karabalin Ruslan <karabalin112@gmail.com>
2025-05-29 20:14:37 +0600

    fix: searching only for active

--- backend/src/main/kotlin/org/karabalin/rentify/configuration/RentalListingSpecification.kt
+++ backend/src/main/kotlin/org/karabalin/rentify/configuration/RentalListingSpecification.kt
@@ -1,6 +1,8 @@
 package org.karabalin.rentify.configuration
 
+import jakarta.persistence.criteria.Predicate
 import org.karabalin.rentify.model.entity.RentalListingEntity
+import org.karabalin.rentify.model.entity.RentalListingStatusEntity
 import org.springframework.data.jpa.domain.Specification
 import org.springframework.stereotype.Component
 
@@ -8,14 +10,22 @@ import org.springframework.stereotype.Component
 class RentalListingSpecification {
     fun searchSpecification(searchQuery: String): Specification<RentalListingEntity> =
         Specification { root, _, cb ->
-            if (searchQuery.isBlank()) {
-                cb.conjunction()
-            } else {
+            val predicates = mutableListOf<Predicate>()
+
+            predicates.add(
+                cb.equal(root.get<RentalListingStatusEntity>("rentalListingStatusEntity").get<String>("name"), "ACTIVE")
+            )
+
+            if (searchQuery.isNotBlank()) {
                 val searchPattern = "%${searchQuery.lowercase()}%"
-                cb.or(
-                    cb.like(cb.lower(root.get("title")), searchPattern),
-                    cb.like(cb.lower(root.get("description")), searchPattern),
+                predicates.add(
+                    cb.or(
+                        cb.like(cb.lower(root.get("title")), searchPattern),
+                        cb.like(cb.lower(root.get("description")), searchPattern)
+                    )
                 )
             }
+
+            cb.and(*predicates.toTypedArray())
         }
 }
